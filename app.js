041c92bb6367b1a42f64208958d30f7696c59d40
Ahmad Tsaqif <ahmadtsaqif007@gmail.com>
2023-09-25 22:00:40 +0700

    index html fix

---
+++ app.js
@@ -0,0 +1,4 @@
+setTimeout(function() {
+    console.log('Adding class is-visible');
+    document.querySelector('.fade-in-section').classList.add('is-visible');
+  }, 1000);
--- scroll.js
+++
@@ -1,5 +0,0 @@
-var scroll = document.querySelector('.scroll');
-window.addEventListener('scroll', function() {
-    var value = window.scrollY;
-    scroll.style.transfrom = `scaleY(${value})`;
-})
